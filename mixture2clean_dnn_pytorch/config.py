649d8f0551b88f401dd34cc95364f7508dd734b1
Chris Hemingway <chris.hemingway@gmail.com>
2019-07-11 21:13:27 +0100

    Major Restructure, move into top level dir

--- mixture2clean_dnn_pytorch/config.py
+++
@@ -1,4 +0,0 @@
-sample_rate = 16000
-fft_size = 512
-hop_size = 256
-window_type = 'hamming'
--- mixture2clean_dnn_pytorch/test9.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import torch
-from torch.autograd import Variable
-import math
-
-
-
-
-a = 1. + 1j
