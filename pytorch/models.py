eff610893d98b3daf9c5d2c567cf529799a3ad94
Chris Hemingway <chris.hemingway@gmail.com>
2019-08-10 10:42:39 +0100

    Tidy up, remove unused functions and collect others

--- pytorch/models.py
+++ pytorch/models.py
@@ -8,52 +8,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
-def move_data_to_gpu(x, cuda):
-
-    if 'float' in str(x.dtype):
-        x = torch.Tensor(x)
-
-    elif 'int' in str(x.dtype):
-        x = torch.LongTensor(x)
-
-    else:
-        raise Exception("Error!")
-
-    if cuda:
-        x = x.cuda()
-
-    return x
-
-
-def init_layer(layer):
-    """Initialize a Linear or Convolutional layer. 
-    Ref: He, Kaiming, et al. "Delving deep into rectifiers: Surpassing 
-    human-level performance on imagenet classification." Proceedings of the 
-    IEEE international conference on computer vision. 2015.
-    """
-    
-    if layer.weight.ndimension() == 4:
-        (n_out, n_in, height, width) = layer.weight.size()
-        n = n_in * height * width
-        
-    elif layer.weight.ndimension() == 2:
-        (n_out, n) = layer.weight.size()
-
-    std = math.sqrt(2. / n)
-    scale = std * math.sqrt(3.)
-    layer.weight.data.uniform_(-scale, scale)
-
-    if layer.bias is not None:
-        layer.bias.data.fill_(0.)
-
-
-def init_bn(bn):
-    """Initialize a Batchnorm layer. """
-    
-    bn.bias.data.fill_(0.)
-    bn.weight.data.fill_(1.)
-    
     
 class DNN(nn.Module):
     def __init__(self, n_concat, freq_bins):
@@ -68,13 +22,42 @@ class DNN(nn.Module):
         self.fc4 = nn.Linear(hidden_units, freq_bins)
         
         self.init_weights()
+
+    @staticmethod
+    def init_layer(layer):
+        """Initialize a Linear or Convolutional layer. 
+        Ref: He, Kaiming, et al. "Delving deep into rectifiers: Surpassing 
+        human-level performance on imagenet classification." Proceedings of the 
+        IEEE international conference on computer vision. 2015.
+        """
+        
+        if layer.weight.ndimension() == 4:
+            (n_out, n_in, height, width) = layer.weight.size()
+            n = n_in * height * width
+            
+        elif layer.weight.ndimension() == 2:
+            (n_out, n) = layer.weight.size()
+    
+        std = math.sqrt(2. / n)
+        scale = std * math.sqrt(3.)
+        layer.weight.data.uniform_(-scale, scale)
+    
+        if layer.bias is not None:
+            layer.bias.data.fill_(0.)
+    
+    @staticmethod
+    def init_bn(bn):
+        """Initialize a Batchnorm layer. """
+        
+        bn.bias.data.fill_(0.)
+        bn.weight.data.fill_(1.)
         
     def init_weights(self):
         
-        init_layer(self.fc1)
-        init_layer(self.fc2)
-        init_layer(self.fc3)
-        init_layer(self.fc4)
+        self.init_layer(self.fc1)
+        self.init_layer(self.fc2)
+        self.init_layer(self.fc3)
+        self.init_layer(self.fc4)
         
     def forward(self, input):
         
